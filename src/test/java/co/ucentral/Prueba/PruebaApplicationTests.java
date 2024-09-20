45d38e28808a271dd8ca1fc2d3a87f88c948379e
ylizaro <ylizaror@ucentral.com>
2024-09-19 19:27:09 -0500

    Anexos

---
+++ src/test/java/co/ucentral/Prueba/PruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ucentral.Prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
